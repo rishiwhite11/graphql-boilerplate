79b92d72552e7a94c9187e1c8357fda1f3aa5c19
Mom <mayukhbasu@Moms-MacBook-Air.local>
2020-04-06 08:39:25 +0530

    added schema stiching

---
+++ src/modules/register/resolver.ts
@@ -0,0 +1,24 @@
+import { ResolverMap } from "../../types/graphql-utils";
+import * as  bcrypt from 'bcryptjs';
+import { User } from "../../entity/User";
+
+export const resolvers: ResolverMap = {
+  Query: {
+    bye: () => "bye"
+  },
+  Mutation: {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword
+      });
+
+      await user.save();
+      return true;
+    }
+  }
+};
--- src/startServer.ts
+++ src/startServer.ts
@@ -1,15 +1,30 @@
-import { GraphQLServer } from 'graphql-yoga'
 import { importSchema } from "graphql-import";
-import { resolvers } from "./resolvers";
-import { createConnection } from "typeorm";
+import { GraphQLServer } from "graphql-yoga";
+import * as path from "path";
+import * as fs from "fs";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
 import { createTypeormConnection } from "./utils/createTypeormConnection";
 
-const typeDefs = importSchema(__dirname+"/schema.graphql")
 
-export const startServer = async() => {
-    const server = new GraphQLServer({ typeDefs, resolvers });
-    await createTypeormConnection();
-    const app = await server.start({port: process.env.NODE_ENV === 'test'? 0: 4000});
-    console.log('Server is running on localhost:4000');
-    return app;
-}
+
+export const startServer = async () => {
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolver.ts`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
+
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
+  await createTypeormConnection();
+  const app = await server.start({
+    port: process.env.NODE_ENV === "test" ? 0 : 4000
+  });
+  console.log("Server is running on localhost:4000");
+
+  return app;
+};
